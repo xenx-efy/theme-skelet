f09be3669a8d8a0b70cc25c4b0da6dbfa80506c6
Alexander Lubovsky <alexander.lubovsky@intexsoft.by>
2021-03-26 17:56:47 +0300

    Configure assets and add examples

--- web/app/themes/starter-theme-1.x/app/assets-setup.php
+++ web/app/themes/starter-theme-1.x/app/assets-setup.php
@@ -6,11 +6,9 @@ use Timber\Theme;
 
 add_action( 'wp_enqueue_scripts', function () {
 	$theme = new Theme();
-	if ( is_single() ) {
-		wp_enqueue_style( 'bts/style.css', asset_path( 'styles/style.css' ), false, filemtime( $theme->path() . '/dist/styles/style.css' ) );
-		wp_enqueue_script( 'bts/script.js', asset_path( 'scripts/script.js' ), [ 'jquery' ], filemtime( $theme->path() . '/dist/scripts/script.js' ), true );
-		wp_localize_script( 'bts/script.js', 'ajax', [
+		wp_enqueue_style( 'timbercss', asset_path( 'styles/style.css' ), false,  1.0);
+		wp_enqueue_script( 'timberjs', asset_path( 'scripts/script.js' ), [ 'jquery' ], 1.0, true );
+		wp_localize_script( 'timberajax', 'ajax', [
 			'ajaxurl' => admin_url( 'admin-ajax.php' ),
 		] );
-	}
 }, 100 );
